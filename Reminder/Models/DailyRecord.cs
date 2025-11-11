0ddcedc34581dd9cd9347163a30ba8882c6d21c0
EduardOrellana <eduardorellana13@gmail.com>
2025-11-10 19:45:57 -0600

    Second Part

--- Reminder/Models/DailyRecord.cs
+++ Reminder/Models/DailyRecord.cs
@@ -2,6 +2,7 @@ namespace Reminder.Data
 {
     public class DailyRecord
     {
+        public int UserID { get; set; }
         public int Id { get; set; }
         public DateTime Date { get; set; }
         public string? Title { get; set; }
--- Reminder/Models/TodoItem.cs
+++ Reminder/Models/TodoItem.cs
@@ -2,6 +2,7 @@ namespace Reminder.Data
 {
     public class TodoItem
     {
+        public int UserID { get; set; }
         public int Id { get; set; }
         public string? Title { get; set; }
         public bool IsDone { get; set; }
